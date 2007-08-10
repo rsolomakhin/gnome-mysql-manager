ce8de60c2a3e202820d837f60f3a831418282f4e
Rouslan.Solomakhin <Rouslan.Solomakhin@b596c888-311a-0410-ae97-858a043f292a>
2007-08-10 23:41:57 +0000

    Added network testing.

--- experiment.py
+++ experiment.py
@@ -3,9 +3,14 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import socket
+import threading
+import gobject
 
 class MysqlManager:
 	def __init__(self):
+		gobject.threads_init()
+		gtk.gdk.threads_init()
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("delete_event", self.delete)
 		self.window.connect("destroy", self.destroy)
@@ -30,8 +35,10 @@ class MysqlManager:
 		self.hostEntry.set_text("localhost")
 		self.userEntry.set_text("root")
 		
-		self.hostEntry.connect("key-release-event", self.update_login_widget)
-		self.userEntry.connect("key-release-event", self.update_login_widget)
+		self.hostEntry.connect("key_release_event", self.update_login_widget)
+		self.userEntry.connect("key_release_event", self.update_login_widget)
+		#self.portSpinr.connect("key_release_event", self.update_login_widget)
+		self.portAdjst.connect("value_changed", self.update_login_widget)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("Host:"))
@@ -59,35 +66,64 @@ class MysqlManager:
 			
 		return self.login_widget
 
-	def login(self, widget, data=None):
-		errors = 0
-		if self.hostEntry.get_text() == "":
-			errors = errors+1
-		if self.userEntry.get_text() == "":
-			errors = errors+1
-		if errors > 0:
+	def login_thread(self):
+		gtk.gdk.threads_enter()
+		host = self.hostEntry.get_text()
+		port = int(self.portAdjst.get_value())
+		user = self.userEntry.get_text()
+		pswd = self.passEntry.get_text()
+		title = user+"@"+host+":"+str(port)
+		# ccheck host and port
+		conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			conn.connect((host, port))
+		except socket.error, (num, message):
+			self.login_widget.set_sensitive(True)
+			self.status.set_text(message+".")
+			orange = gtk.gdk.color_parse("#F57900")
+			if num == 111:
+				self.portSpinr.modify_base(gtk.STATE_NORMAL, orange)
+			elif num == -5 or num == -2 or num == 113:
+				self.hostEntry.modify_base(gtk.STATE_NORMAL, orange)
+			else:
+				print "Error #", num
+				self.portSpinr.modify_base(gtk.STATE_NORMAL, orange)
+				self.hostEntry.modify_base(gtk.STATE_NORMAL, orange)
+			gtk.gdk.threads_leave()
 			return
+		conn.close()
 		view = gtk.TreeView()
-		title = self.userEntry.get_text()+"@"+self.hostEntry.get_text()+":"+str(int(self.portAdjst.get_value()))
 		self.notebook.prepend_page(view, gtk.Label(title))
 		view.show_all()
 		self.notebook.set_current_page(0)
+		self.login_widget.set_sensitive(True)
+		self.status.set_text("")
+		gtk.gdk.threads_leave()
+		return
+
+	def login(self, widget, data=None):
+		self.login_widget.set_sensitive(False)
+		self.status.set_text("Connecting...")
+		thread = threading.Thread(target=self.login_thread)
+		thread.start()
 		
-	def update_login_widget(self, widget, event):
-		red = gtk.gdk.color_parse("red")
-		white = gtk.gdk.color_parse("white")
-		errors = 0
+	def update_login_widget(self, widget, event=None):
+		yellow = gtk.gdk.color_parse("#FCE94F")
+		white = gtk.gdk.color_parse("#FFFFFF")
+		errors = ""
 		if self.hostEntry.get_text() == "":
-			self.hostEntry.modify_base(gtk.STATE_NORMAL, red)
-			errors += 1
+			self.hostEntry.modify_base(gtk.STATE_NORMAL, yellow)
+			errors += "Hostname missing. "
 		else:
 			self.hostEntry.modify_base(gtk.STATE_NORMAL, white)
 		if self.userEntry.get_text() == "":
-			self.userEntry.modify_base(gtk.STATE_NORMAL, red)
-			errors += 1
+			self.userEntry.modify_base(gtk.STATE_NORMAL, yellow)
+			errors += "Username missing. "
 		else:
 			self.userEntry.modify_base(gtk.STATE_NORMAL, white)
-		self.login_button.set_sensitive(errors == 0)
+		self.portSpinr.modify_base(gtk.STATE_NORMAL, white)
+		self.login_button.set_sensitive(len(errors) == 0)
+		self.status.set_text(errors)
 
 	def main(self):
 		gtk.main()
