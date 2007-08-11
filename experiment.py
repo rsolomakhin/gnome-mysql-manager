5b152df0d06d8dcaf8c7097a843cc1a1ae597650
Rouslan.Solomakhin <Rouslan.Solomakhin@b596c888-311a-0410-ae97-858a043f292a>
2007-08-11 03:15:22 +0000

    Added cancel action to login procedure.

--- experiment.py
+++ experiment.py
@@ -5,11 +5,9 @@ pygtk.require('2.0')
 import gtk
 import socket
 import threading
-import gobject
 
 class MysqlManager:
 	def __init__(self):
-		gobject.threads_init()
 		gtk.gdk.threads_init()
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("delete_event", self.delete)
@@ -26,6 +24,9 @@ class MysqlManager:
 
 	def build_login_widget(self):
 		self.login_widget = gtk.VBox()
+		
+		self.login_data_box = gtk.VBox()
+
 		self.hostEntry = gtk.Entry()
 		self.portAdjst = gtk.Adjustment(value=3306, lower=1, upper=64000, step_incr=1)
 		self.portSpinr = gtk.SpinButton(self.portAdjst)
@@ -37,48 +38,55 @@ class MysqlManager:
 		
 		self.hostEntry.connect("key_release_event", self.update_login_widget)
 		self.userEntry.connect("key_release_event", self.update_login_widget)
-		#self.portSpinr.connect("key_release_event", self.update_login_widget)
 		self.portAdjst.connect("value_changed", self.update_login_widget)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("Host:"))
 		box.add(self.hostEntry)
-		self.login_widget.add(box)
+		self.login_data_box.add(box)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("Port:"))
 		box.add(self.portSpinr)
-		self.login_widget.add(box)
+		self.login_data_box.add(box)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("User:"))
 		box.add(self.userEntry)
-		self.login_widget.add(box)
+		self.login_data_box.add(box)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("Password:"))
 		box.add(self.passEntry)
-		self.login_widget.add(box)
+		self.login_data_box.add(box)
 
 		self.login_button = gtk.Button("Login")
 		self.login_button.connect("clicked", self.login)
-		self.login_widget.add(self.login_button)
-			
+		self.cancel_button = gtk.Button("Cancel")
+		self.cancel_button.connect("clicked", self.cancel_login)
+		self.cancel_button.set_sensitive(False)
+		buttons = gtk.HButtonBox()
+		buttons.add(self.cancel_button)
+		buttons.add(self.login_button)
+		
+		self.login_widget.add(self.login_data_box)
+		self.login_widget.add(buttons)
+		
 		return self.login_widget
 
 	def login_thread(self):
-		gtk.gdk.threads_enter()
 		host = self.hostEntry.get_text()
 		port = int(self.portAdjst.get_value())
 		user = self.userEntry.get_text()
 		pswd = self.passEntry.get_text()
 		title = user+"@"+host+":"+str(port)
 		# ccheck host and port
-		conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		gtk.gdk.threads_enter()
+		self.test_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
-			conn.connect((host, port))
+			self.test_conn.connect((host, port))
 		except socket.error, (num, message):
-			self.login_widget.set_sensitive(True)
+			self.set_connecting_status(False)
 			self.status.set_text(message+".")
 			orange = gtk.gdk.color_parse("#F57900")
 			if num == 111:
@@ -91,7 +99,8 @@ class MysqlManager:
 				self.hostEntry.modify_base(gtk.STATE_NORMAL, orange)
 			gtk.gdk.threads_leave()
 			return
-		conn.close()
+		self.test_conn.close()
+		gtk.gdk.threads_leave()
 		
 		databases = gtk.TreeStore(str)
 		databases.append(None, ["hello"])
@@ -108,17 +117,28 @@ class MysqlManager:
 		self.notebook.prepend_page(view, gtk.Label(title))
 		self.notebook.show_all()
 		self.notebook.set_current_page(0)
-		self.login_widget.set_sensitive(True)
-		self.status.set_text("")
-		gtk.gdk.threads_leave()
+		self.set_connecting_status(False)
 		return
 
+	def set_connecting_status(self, is_connecting):
+		self.login_data_box.set_sensitive(not is_connecting)
+		self.login_button.set_sensitive(not is_connecting)
+		self.cancel_button.set_sensitive(is_connecting)
+		if is_connecting:
+			self.status.set_text("Connecting...")
+		else:
+			self.status.set_text("")
+
+	def cancel_login(self, widget, data=None):
+		self.conn_thread.stop()
+		self.test_conn.close()
+		self.set_connecting_status(False)		
+
 	def login(self, widget, data=None):
-		self.login_widget.set_sensitive(False)
-		self.status.set_text("Connecting...")
-		thread = threading.Thread(target=self.login_thread)
-		thread.start()
-		
+		self.set_connecting_status(True)
+		self.conn_thread = threading.Thread(target=self.login_thread)
+		self.conn_thread.start()
+
 	def update_login_widget(self, widget, event=None):
 		yellow = gtk.gdk.color_parse("#FCE94F")
 		white = gtk.gdk.color_parse("#FFFFFF")
