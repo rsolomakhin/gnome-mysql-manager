5d01c6bd9a7b831f98934566ec228ede84193b09
Rouslan.Solomakhin <Rouslan.Solomakhin@b596c888-311a-0410-ae97-858a043f292a>
2007-08-15 19:52:50 +0000

    Now can connect to database and retreive a list of databases

--- experiment.py
+++ experiment.py
@@ -6,7 +6,7 @@ import gtk
 import gconf
 import gobject
 import socket
-import threading
+import MySQLdb
 
 class MysqlManagerGui:
 	"GNOME MySQL Manager"
@@ -104,8 +104,9 @@ class MysqlManagerGui:
 		self.userEntry.set_text("root")
 		
 		self.hostEntry.connect("changed", self.update_login_widget)
-		self.userEntry.connect("changed", self.update_login_widget)
 		self.portSpinr.connect("changed", self.update_login_widget)
+		self.userEntry.connect("changed", self.update_login_widget)
+		self.passEntry.connect("changed", self.update_login_widget)
 
 		box = gtk.HBox()
 		box.add(gtk.Label("Host:"))
@@ -138,7 +139,6 @@ class MysqlManagerGui:
 		return self.login_widget
 
 	def on_login_button_clicked(self, widget, data=None):
-		self.set_connecting_status(True)
 		host = self.hostEntry.get_text()
 		port = int(self.portAdjst.get_value())
 		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -150,28 +150,40 @@ class MysqlManagerGui:
 			connection_error = error
 		finally:
 			self.connection.close()
-		self.set_connecting_status(False)
 		if connection_error is None:
 			self.login()
 		else:
 			try:
 				error_message = connection_error[len(connection_error)-1]
 			except TypeError: # TypeError="Object 'timeout' has no len()"
-				error_message = 'Timed out'
+				error_message = "Connection timed out"
 			self.status.set_text(error_message+".")
 			self.portSpinr.modify_base(gtk.STATE_NORMAL, self.conn_error_color)
 			self.hostEntry.modify_base(gtk.STATE_NORMAL, self.conn_error_color)
 
 	def login(self):
+		"Login to the MySQL server and display all databases"
+
+		databases = gtk.TreeStore(str)
 		host = self.hostEntry.get_text()
 		port = int(self.portAdjst.get_value())
 		user = self.userEntry.get_text()
 		pswd = self.passEntry.get_text()
 		title = user+"@"+host+":"+str(port)
 		
-		databases = gtk.TreeStore(str)
-		databases.append(None, ["hello"])
-		databases.append(None, ["good bye"])
+		try:
+			server = MySQLdb.connect(host=host, port=port, user=user, passwd=pswd)
+		except MySQLdb.MySQLError, error:
+			self.status.set_text(error[1])
+			return
+		cursor = server.cursor()
+		cursor.execute("show databases")
+		row = cursor.fetchone()
+		while row is not None:
+			databases.append(None, [row[0]])
+			row = cursor.fetchone()
+		cursor.close()
+
 		view = gtk.TreeView(databases)
 		column = gtk.TreeViewColumn("Database")
 		view.append_column(column)
@@ -186,13 +198,13 @@ class MysqlManagerGui:
 		self.notebook.set_current_page(0)
 		return
 
-	def set_connecting_status(self, is_connecting):
-		self.login_data_box.set_sensitive(not is_connecting)
-		self.login_button.set_sensitive(not is_connecting)
-		if is_connecting:
-			self.status.set_text("Connecting...")
-		else:
-			self.status.set_text("")
+	#def set_connecting_status(self, is_connecting):
+	#	self.login_data_box.set_sensitive(not is_connecting)
+	#	self.login_button.set_sensitive(not is_connecting)
+	#	if is_connecting:
+	#		self.status.set_text("Connecting...")
+	#	else:
+	#		self.status.set_text("")
 
 	def update_login_widget(self, widget, event=None):
 		errors = ""
